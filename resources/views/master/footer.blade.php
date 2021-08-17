dff9cee2fcbf56db6881f89d15b98ebd1ec85744
Amdadul Hoque <amdadulhaque05@gmail.com>
2021-08-18 01:37:24 +0600

    template mastering done

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer footer-static footer-dark navbar-border">
+    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
+      <span class="float-md-left d-block d-md-inline-block">Copyright &copy; <?php echo date('Y'); ?>  <a class="text-bold-800 grey darken-2" href="https://github.com/amdadul"
+                                                                                     target="_blank">Amdadul Hoque </a>, All rights reserved. </span>
+        <span class="float-md-right d-block d-md-inline-block d-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span>
+    </p>
+</footer>
+
